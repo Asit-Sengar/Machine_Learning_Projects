7551e9b10df4ae7cc4b4276caf9af238791c85b4
Asit Sengar <asitsengar28@gmail.com>
2026-08-25 17:16:30 +0530

    added requirements.txt

--- Heart_disease_prediction/app.py
+++ Heart_disease_prediction/app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 import pandas as pd
 import pickle
+
+with open("disease_prediction.pkl", "rb") as file:
+    model = pickle.load(file)
+
+st.title("Heart Disease Prediction")
