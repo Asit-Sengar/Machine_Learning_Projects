e09a477695374d4dac73f51ce6c10d295505f784
Asit Sengar <asitsengar28@gmail.com>
2026-09-07 11:57:36 +0530

    trying to make app.py file on my own

--- Heart_disease_prediction/app.py
+++ Heart_disease_prediction/app.py
@@ -1,5 +1,29 @@
 from pathlib import Path
 import pickle
 
+# this is for opening the trained model with the file
+with open("disease_prediction.pkl", "rb") as file:
+    model = pickle.load(file)  # now we will use the name model
+
+
 import pandas as pd
 import streamlit as st
+
+st.set_page_config(  # this is for setting the page configuration
+    page_title="Heart Disease Prediction",
+    page_icon="💀"
+)
+
+st.title("Heart Disease Prediction")
+st.write("Please enter patient's information")
+
+age = st.number_input(
+    "Age",  # this is the exact column name of the dataframe which the model is trained upon
+    min_value=1,
+    max_value=100
+)
+
+gender = st.selectbox(
+    "Gender",
+    ["Male", "Female"]
+)
